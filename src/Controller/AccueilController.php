47d636425eaa5bb180805c354e0ebbc7b69afec0
Ahmed Sayeh <Ahmedd>
2025-02-03 15:10:49 +0100

    implementation du code pour ajouter ticket + connexion/inscription

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Ticket;
+use App\Form\FormTicketType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
-final class AccueilController extends AbstractController
+class AccueilController extends AbstractController
 {
-    #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function __construct(private EntityManagerInterface $entityManager)
     {
+    }
+
+    #[Route('/', name: 'app_accueil')]
+    public function index(Request $request): Response
+    {
+        $ticket = new Ticket();
+        $form = $this->createForm(FormTicketType::class, $ticket);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($ticket);
+            $this->entityManager->flush();
+
+            // Redirect or display success message
+        }
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'form' => $form->createView()
         ]);
     }
+
 }
